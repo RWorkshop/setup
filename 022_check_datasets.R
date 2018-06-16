f727ea5e86000df56e7f85dd21bf331f12a9a7dc
DragonflyStats <kobriendublin@gmail.com>
2018-06-16 15:53:55 +0100

    Update 022_check_datasets.R

--- 022_check_datasets.R
+++ 022_check_datasets.R
@@ -1,1 +1,11 @@
+
+####### Data sets from Github
+
 if ( !("Superworld.csv" %in% list.files()) ){cat("Superworld CSV files not present in working directory.\n")}
+
+####### Data sets for R packages
+
+if ( !("diamonds" %in% ls() ) ){cat("Diamonds data set (from ggplot2 package) is not available in R environment.\n")}
+if ( !("midwest" %in% ls() ) ){cat("Midwest data set (from ggplot2 package) is not available in R environment.\n")}
+if ( !("txhousing" %in% ls() ) ){cat("Texas Housing data set (from ggplot2 package) is not available in R environment.\n")}
+
