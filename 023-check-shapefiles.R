51b0fe6922588c0e723fe67e0f2e568893307649
DragonflyStats <kobriendublin@gmail.com>
2018-06-16 15:45:33 +0100

    Create 023-check-shapefiles.R

---
+++ 023-check-shapefiles.R
@@ -0,0 +1,4 @@
+
+if ( !("IRL_adm1.shp" %in% list.files()) ){cat("Ireland Counties shapefile not present in working directory.\n")}
+if ( !("USA_adm1.shp" %in% list.files()) ){cat("USA States shapefile not present in working directory.\n")}
+if ( !("USA_adm2.shp" %in% list.files()) ){cat("USA Counties shapefile not present in working directory.\n")}
