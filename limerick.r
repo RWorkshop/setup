2eeaa7e984bc34cb225d2bd235325aada2cbbe1e
DragonflyStats <kobriendublin@gmail.com>
2018-06-16 17:27:07 +0100

    Create limerick.r

---
+++ limerick.r
@@ -0,0 +1,4 @@
+52.6629196,-8.6277966, "Mickey Martin's", "Pub"
+52.6622911,-8.6310300, "La Cucina Centro", "Restaurant"
+52.6583858,-8.6328818, "south's Bar", "Pub"
+52.6672134,-8.6241657, "The Locke Bar", "Pub"
