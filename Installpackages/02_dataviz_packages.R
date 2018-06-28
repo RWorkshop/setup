959d1ca36294325267297ec04785a82567dca82f
DragonflyStats <kobriendublin@gmail.com>
2018-06-28 21:27:33 +0100

    Update 02_dataviz_packages.R

--- Installpackages/02_dataviz_packages.R
+++ Installpackages/02_dataviz_packages.R
@@ -1,4 +1,8 @@
 
 package_list <- c("ggcorrplot","WVPlots","GGally")
 
+already_installed <- installed.packages()
 
+install_these <- setdiff(package_list,already_installed)
+
+if( length(install_these) >0 ){ install.packages(install_these) }
