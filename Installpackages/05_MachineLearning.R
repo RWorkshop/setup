bd31bb7c02e43408243a44b726f7c08083bed534
DragonflyStats <kobriendublin@gmail.com>
2018-06-28 21:35:17 +0100

    Update 05_MachineLearning.R

--- Installpackages/05_MachineLearning.R
+++ Installpackages/05_MachineLearning.R
@@ -1,4 +1,8 @@
 
-ML_list <- c("RandomForest","MLbench","ElemStatLearn")
+package_list <- c("RandomForest","MLbench","ElemStatLearn")
 
-install.packages(ML_list, repos = "https://ftp.heanet.ie/mirrors/cran.r-project.org/")
+already_installed <- installed.packages()
+
+install_these <- setdiff(package_list, already_installed)
+
+if( length(install_these) >0 ){ install.packages(install_these,repos = "https://ftp.heanet.ie/mirrors/cran.r-project.org/") }
