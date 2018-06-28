d09286ffc48446a852241d11a8dd0c5e5d4ee8ef
DragonflyStats <kobriendublin@gmail.com>
2018-06-28 21:25:49 +0100

    Update 01_tidyverse.R

--- Installpackages/01_tidyverse.R
+++ Installpackages/01_tidyverse.R
@@ -4,4 +4,4 @@ tidyverse_list <- c("dplyr","magrittr","broom","tidyr","modelr","recipes","purrr
 
 install_these <- setdiff(tidyverse_list, already_installed)
 
-install.packages(install_these,repos = "https://ftp.heanet.ie/mirrors/cran.r-project.org/")
+if( length(install_these) >0 ){install.packages(install_these,repos = "https://ftp.heanet.ie/mirrors/cran.r-project.org/")}
